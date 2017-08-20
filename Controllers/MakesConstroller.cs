b094b455b3b25d1d5e963e412e2f17ecb8c95131
Filip Laurentiu <f.laurentiu26@gmail.com>
2017-08-20 18:11:53 +0300

    Before using AutoMapper

---
+++ Controllers/MakesConstroller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using vega.Models;
+using vega.Persistance;
+
+namespace vega.Controllers
+{
+    public class MakesConstroller : Controller
+    {
+        private readonly VegaDbContext _context;
+
+        public MakesConstroller(VegaDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("/api/makes")]
+        public async Task<IEnumerable<Make>> GetMakesAsync()
+        {
+            return await _context.Makes.Include(m => m.Models).ToListAsync();
+        }
+    }
+}
